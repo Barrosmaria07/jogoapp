0e8e233ab7056b73c8d075de662109a580309e91
jogoapp <duduzinhacb@gmal.com>
2024-03-05 09:45:50 -0300

    primeiro commiti

---
+++ jogoapp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace jogoapp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
